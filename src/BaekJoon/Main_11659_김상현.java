6c25fd890f86d93faf5d40a2255f4b82b8c026ea
김상현 <ksh3967@naver.com>
2022-05-18 00:13:30 +0900

    0517 commit

---
+++ src/BaekJoon/Main_11659_김상현.java
@@ -0,0 +1,33 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_11659_김상현 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N+1];
+		st = new StringTokenizer(br.readLine());
+		arr[0] = 0;
+		for(int i=1;i<=N;i++) {
+			arr[i] = arr[i-1] + Integer.parseInt(st.nextToken());
+		}
+		for(int i=0;i<M;i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			System.out.println(arr[end]-arr[start-1]);
+		}
+		
+	}
+
+}
+
+// 5 9 12 14 15
