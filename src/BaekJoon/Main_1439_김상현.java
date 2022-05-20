aaa0cc015d792d02571a67a8e969bb3cb1aa3776
김상현 <ksh3967@naver.com>
2022-05-21 02:55:02 +0900

    0520 commit

---
+++ src/BaekJoon/Main_1439_김상현.java
@@ -0,0 +1,24 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_1439_김상현 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		int cnt = 1;
+		for(int i=0;i<str.length()-1;i++) {
+			if(str.charAt(i)!=str.charAt(i+1)) {
+				cnt++;
+			}
+		}
+		System.out.println(cnt/2);
+		
+		
+	}
+
+}
