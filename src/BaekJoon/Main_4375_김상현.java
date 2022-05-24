eca797b6c9836ac14a77c21f5edf8284259d71a0
김상현 <ksh3967@naver.com>
2022-05-25 01:16:21 +0900

    0525 commit

---
+++ src/BaekJoon/Main_4375_김상현.java
@@ -0,0 +1,28 @@
+package BaekJoon;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main_4375_김상현 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNextInt()) {
+			int num = sc.nextInt();
+			int ans = 1;
+			int x=1;
+			while(true) {
+				if(x%num==0) {
+					System.out.println(ans);
+					break;
+				}
+				x %= num;
+				x = x * 10 + 1;
+				ans++;
+			}
+		}
+		
+	}
+
+}
