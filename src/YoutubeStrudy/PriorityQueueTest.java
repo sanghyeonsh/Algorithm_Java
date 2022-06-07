ad4378664c213a9f657b72b9ac37b004b425a1e7
김상현 <ksh3967@naver.com>
2022-06-08 08:54:28 +0900

    0608 commit

--- src/YoutubeStrudy/PriorityQueueTest.java
+++ src/YoutubeStrudy/PriorityQueueTest.java
@@ -4,7 +4,7 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class PriorityQueueTest {
-
+ 
 	public static void main(String[] args) {
 		
 		PriorityQueue<Integer> minQueue = new PriorityQueue<Integer>();
