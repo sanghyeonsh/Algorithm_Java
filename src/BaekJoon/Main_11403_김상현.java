3fd4a7b567ae0474e79c371f7753505c354936b5
김상현 <ksh3967@naver.com>
2022-06-18 01:19:10 +0900

    0618 commit

---
+++ src/BaekJoon/Main_11403_김상현.java
@@ -0,0 +1,44 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_11403_김상현 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		int[][] map = new int[N][N];
+		for(int i=0;i<N;i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0;j<N;j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		for(int k=0;k<N;k++) {
+			for(int i=0;i<N;i++) {
+				for(int j=0;j<N;j++) {
+					if(map[i][k]==1 && map[k][j]==1) {
+						map[i][j]=1;
+					}
+				}
+			}
+		}
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<N;j++) {
+				if(map[i][j]==1) {
+					System.out.print(1+" ");
+				}else {
+					System.out.print(0+" ");
+				}
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
