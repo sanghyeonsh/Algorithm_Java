ffc772869751a7a07dad798118273170b0fc7bc1
김상현 <ksh3967@naver.com>
2022-07-10 14:24:48 +0900

    0710 commit

--- src/BaekJoon/Main_16935_김상현.java
+++ src/BaekJoon/Main_16935_김상현.java
@@ -112,7 +112,7 @@ public class Main_16935_김상현 {
 				for(int i=0;i<N/2;i++) {
 					for(int j=M/2;j<M;j++) {
 						impl[i][j]=map[i+N/2][j];
-					}
+					} 
 				}
 				for(int i=N/2;i<N;i++) {
 					for(int j=0;j<M/2;j++) {
