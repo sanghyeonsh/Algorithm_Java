ad5770f2418df81833e9ac82b02b42c79e2990ea
ksh3967 <ksh3967@naver.com>
2022-10-17 01:25:30 +0900

    1017 commit

--- src/BaekJoon/Main_2580_김상현.java
+++ src/BaekJoon/Main_2580_김상현.java
@@ -24,8 +24,7 @@ public class Main_2580_김상현 {
 			}
 		}
 		System.out.println(cnt);
-		solve(0,-1,-1);
-		
+		solve(0,-1,-1);		
 	}
 	private static void solve(int check, int r, int c) {
 //		System.out.println(check);
