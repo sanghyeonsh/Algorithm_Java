722ac94a682620c84dd0ba6bdce1caa363e9bad9
sanghyeonsh <ksh3967@naver.com>
2022-11-13 00:14:30 +0900

    1113 commit

---
+++ src/AfterStudy/test.java
@@ -0,0 +1,14 @@
+package AfterStudy;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "1,2,3,";
+		String[] strArr = str.split(",");
+//		for(String s:strArr) {
+//			System.out.println(s);
+//		}
+		System.out.println(strArr.length);
+	}
+}
